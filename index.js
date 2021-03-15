925f8b402aa916d22e93d8750ef13e234accbd01
mart8ins <mart8ins@gmail.com>
2021-03-15 22:24:15 +0200

    finsihed landing page

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ app.engine("ejs", engineMate); // use ejs-locals for all ejs templates
 app.set("views",__dirname + "/views");
 
 
+let categorie = "";
+
 app.get("/", (req, res)=> {
     res.render("home")
+})
+
+app.get("/blog", (req, res)=> {
+    res.render("blog/blog", {categorie})
+})
+
+app.post("/blog", (req, res)=> {
+categorie = req.body.categorie;
+    res.redirect("/blog");
 })
 
 app.listen(3000, ()=> {
