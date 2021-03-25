7403b4439f9e7018ab2d5f9170d7072299c2fc92
mart8ins <mart8ins@gmail.com>
2021-03-25 14:50:40 +0200

    created footer, improved some styleing

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ router.use(loggedUserRouteGuard);
 
 // models
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 // route for logged user to manage his profile, and see other data
 router.route("/profile")
@@ -44,10 +45,44 @@ router.route("/profile")
     res.render("user/profile", {username,email, usersBlogs, recievedComments, leftComments})
 })
 
+
 // route for other logged users to see user who left comment or blog, stats
 router.route("/:username/profile")
-.get((req, res)=> {
-    res.render("user/userData")
+.get(async (req, res)=> {
+    const {username} = req.params;
+    // get all blogs
+    const blogs = await Blog.find().populate("author").populate("comments");
+    // get user data
+    const user = await User.findOne({username: username});
+    
+    
+    // to store users blogs
+    const usersBlogs = [];
+    // to store recieved comments for users blogs
+    const recievedComments = [];
+    // store users left comments
+    const leftComments = [];
+
+    blogs.forEach((blog)=> {
+        // to filter users blogs and recieved comments
+        if(blog.author.username == username) {
+            usersBlogs.push(blog);
+            if(blog.comments.length){
+                blog.comments.forEach((comment)=> {
+                    recievedComments.push(comment)
+                })
+            }
+        }
+        // to get all left by user comments
+        if(blog.comments.length){
+            blog.comments.forEach((comment)=> {
+                if(comment.author == username){
+                    leftComments.push(comment)
+                }
+            })
+        }
+    })
+    res.render("user/user", {username, usersBlogs, recievedComments, leftComments})
 })
 
 module.exports = router;
