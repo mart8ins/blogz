0532e15fc5a7855e5ef0942e3fa386f1f086f8f0
mart8ins <mart8ins@gmail.com>
2021-04-03 16:13:55 +0300

    fixed visual error

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.use(mongoSanitize());
 
 
 // db url 
-// const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/blogzz";
-const dbUrl = "mongodb://localhost:27017/blogzz";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/blogzz";
+// const dbUrl = "mongodb://localhost:27017/blogzz";
 
 /* *********
 MONGO datu bāze
